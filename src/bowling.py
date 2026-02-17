f7892b987d53cf57848ff8fb849e5cfbfe85f632
techrabbit58 <dzb@pruefbit.de>
2026-02-17 11:42:55 +0100

    new: can now score for normal lines and for one or more spares

--- src/bowling.py
+++ src/bowling.py
@@ -1,6 +1,16 @@
-def score(frames: str) -> int:
+def _deliveries(frame: str) -> tuple[int, ...]:
+    frame = frame.replace("-", "0")
+    first = int(frame[0])
+    second = 10 - first if frame.endswith("/") else int(frame[1])
+    return first, second
+
+
+def score(line: str) -> int:
     total_score = 0
-    for c in frames:
-        if c not in "-\n\t\r ":
-            total_score += int(c)
+    frames = line.split()
+    for i, frame in enumerate(frames):
+        if frame[-1] == "/":  # spare
+            total_score += 10 + _deliveries(frames[i + 1])[0]
+        else:
+            total_score += sum(_deliveries(frame))
     return total_score
--- test/test_bowling_game.py
+++ test/test_bowling_game.py
@@ -7,6 +7,9 @@ import bowling
     ("-- -- -- -- -- -- -- -- -- --", 0),
     ("12 12 12 12 12 12 12 12 12 12", 30),
     ("9- 9- 9- 9- 9- 9- 9- 9- 9- 9-", 90),
+    ("6/ 8- -- -- -- -- -- -- -- --", 26),
+    ("5/ 5/ 1- 1- 1- 1- 1- 1- 1- 1-", 34),
+    ("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 54", 144),
 ])
 def test_score(actual_line, expected_score):
     assert bowling.score(actual_line) == expected_score
