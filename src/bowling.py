89147f3ecafa3d3e9520bc2c2216c48e40224313
techrabbit58 <dzb@pruefbit.de>
2026-02-17 11:09:39 +0100

    new: can now score for normal lines with or without misses

--- src/bowling.py
+++ src/bowling.py
@@ -1,2 +1,6 @@
 def score(frames: str) -> int:
-    return 0
+    total_score = 0
+    for c in frames:
+        if c not in "-\n\t\r ":
+            total_score += int(c)
+    return total_score
--- test/test_bowling_game.py
+++ test/test_bowling_game.py
@@ -1,6 +1,12 @@
+import pytest
+
 import bowling
 
 
-def test_all_misses():
-    frames = "-- -- -- -- -- -- -- -- -- --"
-    assert bowling.score(frames) == 0
+@pytest.mark.parametrize("actual_line, expected_score", [
+    ("-- -- -- -- -- -- -- -- -- --", 0),
+    ("12 12 12 12 12 12 12 12 12 12", 30),
+    ("9- 9- 9- 9- 9- 9- 9- 9- 9- 9-", 90),
+])
+def test_score(actual_line, expected_score):
+    assert bowling.score(actual_line) == expected_score
